31560f24cf19f59feaacb94af5cf6384cfd0478c
Cristian <cristianmherrera21@gmail.com>
2024-10-05 19:16:02 -0500

    JwtAuthenticationFilterTest fixed & refactored

--- Infrastructure/src/test/java/Suites/TestSuitesA.java
+++ Infrastructure/src/test/java/Suites/TestSuitesA.java
@@ -4,7 +4,6 @@ import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
-
 @SelectPackages("org.cris6h16")
 public class TestSuitesA {
 }
--- Infrastructure/src/test/java/org/cris6h16/Config/SpringBoot/Security/Filters/JwtAuthenticationFilterTest.java
+++ Infrastructure/src/test/java/org/cris6h16/Config/SpringBoot/Security/Filters/JwtAuthenticationFilterTest.java
@@ -17,6 +17,7 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.io.IOException;
@@ -25,6 +26,7 @@ import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -77,12 +79,7 @@ public class JwtAuthenticationFilterTest {
         // Arrange
         String token = "imthetoken";
         HttpServletRequest request = mock(HttpServletRequest.class);
-        Cookie accessTokenCookie = new Cookie(accessTokenCookieName, token);
-        Cookie other1 = new Cookie("other1", "value of the cookie1");
-        Cookie other2 = new Cookie("other2", "value of the cookie2");
-        Cookie other3 = new Cookie("other3", "value of the cookie3");
-
-        Cookie[] cookies = new Cookie[]{other1, other2, accessTokenCookie, other3};
+        Cookie[] cookies = createCookiesWithToken(token);
 
         when(request.getCookies()).thenReturn(cookies);
 
@@ -97,18 +94,24 @@ public class JwtAuthenticationFilterTest {
         verify(jwtUtilsImpl, times(1)).validate(token);
     }
 
+    private Cookie[] createCookiesWithToken(String token) {
+        Cookie accessTokenCookie = new Cookie(accessTokenCookieName, token);
+        Cookie other1 = new Cookie("other1", "value of the cookie1");
+        Cookie other2 = new Cookie("other2", "value of the cookie2");
+        Cookie other3 = new Cookie("other3", "value of the cookie3");
+
+        return new Cookie[]{accessTokenCookie, other1, other2, other3};
+    }
+
     @Test
     void doFilterInternal_tokenInvalid() throws ServletException, IOException {
         // Arrange
         String invalidToken = "expired,etc";
         FilterChain filterChain = mock(FilterChain.class);
         HttpServletRequest request = mock(HttpServletRequest.class);
-        Cookie accessTokenCookie = new Cookie(accessTokenCookieName, invalidToken);
 
-        when(request.getCookies())
-                .thenReturn(new Cookie[]{accessTokenCookie});
-        when(jwtUtilsImpl.validate(invalidToken))
-                .thenReturn(false);
+        when(request.getCookies()).thenReturn(createCookiesWithToken(invalidToken));
+        when(jwtUtilsImpl.validate(invalidToken)).thenReturn(false);
 
         //Act
         jwtAuthenticationFilter.doFilterInternal(
@@ -127,19 +130,15 @@ public class JwtAuthenticationFilterTest {
     @Test
     void doFilterInternal_tokenValid() throws ServletException, IOException {
         // Arrange
-        SecurityContextHolder.getContext().setAuthentication(null);
+        SecurityContext securityContext = currentMockedSecurityContext();
+        UserDetailsWithId userDetailsWithId = userDWIWithAuthorities();
 
         String validToken = "validToken";
-        Collection<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority("EROLE_USER"));
         FilterChain filterChain = mock(FilterChain.class);
-        HttpServletResponse response = mock(HttpServletResponse.class);
         HttpServletRequest request = mock(HttpServletRequest.class);
-        Cookie accessTokenCookie = new Cookie(accessTokenCookieName, validToken);
 
-        UserDetailsWithId userDetailsWithId = mock(UserDetailsWithId.class);
-        when(userDetailsWithId.getAuthorities()).thenReturn(authorities);
 
-        when(request.getCookies()).thenReturn(new Cookie[]{accessTokenCookie});
+        when(request.getCookies()).thenReturn(createCookiesWithToken(validToken));
         when(jwtUtilsImpl.validate(validToken)).thenReturn(true);
         when(jwtUtilsImpl.getId(validToken)).thenReturn(99L);
         when(userDetailsService.loadUserById(99L)).thenReturn(userDetailsWithId);
@@ -147,19 +146,33 @@ public class JwtAuthenticationFilterTest {
         //Act
         jwtAuthenticationFilter.doFilterInternal(
                 request,
-                response,
+                mock(HttpServletResponse.class),
                 filterChain
         );
 
         // Assert
-        UsernamePasswordAuthenticationToken auth = (UsernamePasswordAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
-        assertEquals(userDetailsWithId, auth.getPrincipal());
-        assertEquals(authorities, auth.getAuthorities());
-
+        verify(securityContext, times(1)).setAuthentication(argThat(authentication -> { // UsernamePasswordAuthenticationToken
+            assertEquals(userDetailsWithId, authentication.getPrincipal());
+            assertNull(authentication.getCredentials());
+            assertEquals(userDetailsWithId.getAuthorities(), authentication.getAuthorities());
+            return true;
+        }));
         verify(jwtUtilsImpl, times(1)).validate(validToken);
         verify(jwtUtilsImpl, times(1)).getId(validToken);
         verify(userDetailsService, times(1)).loadUserById(99L);
-        verify(filterChain, times(1)).doFilter(request, response);
+        verify(filterChain, times(1)).doFilter(any(), any());
+    }
+
+    private UserDetailsWithId userDWIWithAuthorities() {
+        UserDetailsWithId userDetailsWithId = mock(UserDetailsWithId.class);
+        when(userDetailsWithId.getAuthorities()).thenReturn(List.of(new SimpleGrantedAuthority("ROLE_USER")));
+        return userDetailsWithId;
+    }
+
+    private SecurityContext currentMockedSecurityContext() {
+        SecurityContext securityContext = mock(SecurityContext.class);
+        SecurityContextHolder.setContext(securityContext);
+        return securityContext;
     }
 
 
